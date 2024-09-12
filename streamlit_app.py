a0828f17f707a82a14aa692a8640d2063c0dfdcf
Felix Blumentritt <a12028818@unet.univie.ac.at>
2024-09-12 20:14:42 +0200

    passwortschutz

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,34 @@ import pandas as pd
 from PIL import Image
 import plost
 import json
+import hmac
 import base64
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Passwort", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("Passwort falsch")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
