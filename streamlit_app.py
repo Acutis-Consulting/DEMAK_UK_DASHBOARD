f4f8aefcfd005fd45e4f6dafbe24c05652cdcb59
Felix Blumentritt <a12028818@unet.univie.ac.at>
2024-09-23 18:01:03 +0200

    hide git

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,18 @@ import json
 import hmac
 import base64
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 def check_password():
     """Returns `True` if the user had the correct password."""
